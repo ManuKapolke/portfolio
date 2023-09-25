049e1743de3701aa9ce3cdc945387b75aaf18bca
Manu Kapolke <manukapolke@gmail.com>
2023-09-25 15:05:46 +0200

    layoutSesrvice buttons started with above-the-fold

---
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LayoutService } from '../layout.service';
+
+@Component({
+  selector: 'app-above-the-fold',
+  templateUrl: './above-the-fold.component.html',
+  styleUrls: ['./above-the-fold.component.scss']
+})
+export class AboveTheFoldComponent {
+  constructor(public layoutService: LayoutService) { }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  mainColorA = ['#9747FF', '#4B47FF', '#FF4747'];
-  mainColorB = ['#70E61C', '#00BEE8', '#FA7902'];
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.setColorTheme(2);
+
   }
 
-  setColorTheme(version: number): void {
-    // Set the values of the global CSS custom properties
-    document.documentElement.style.setProperty('--mainColorA', this.mainColorA[version - 1]);
-    document.documentElement.style.setProperty('--mainColorB', this.mainColorB[version - 1]);
-    document.documentElement.style.setProperty('--logoA', `url('assets/logo/v${version}_mk-logo_a.png')`);
-    document.documentElement.style.setProperty('--logoB', `url('assets/logo/v${version}_mk-logo_b.png')`);
-  }
 
 }

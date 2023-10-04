3130bee81a01c1a319ea47879c20ea4bce8a5d4d
Manu Kapolke <manukapolke@gmail.com>
2023-10-04 10:27:59 +0200

    finished contact form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -9,53 +9,67 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactFormComponent {
   contactForm!: FormGroup;
-  messageIsSending: boolean = false;
+  sending: boolean = false;
+  messageIsSent: boolean = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.contactForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],//Validators.minLength(2)
       message: ['', [Validators.required, Validators.minLength(2)]],
       checkbox: [false, Validators.requiredTrue]
     });
   }
 
   async sendMail() {
+    this.sending = true;
     this.contactForm.disable();
-    this.messageIsSending = true;
-
-    // Animation anzeigen
-    // todo...
-
-    let formData = new FormData();
-    formData.append('name', this.contactForm.get('name')?.value);
-    formData.append('email', this.contactForm.get('email')?.value);
-    formData.append('message', this.contactForm.get('message')?.value);
+    document.body.style.overflowY = 'hidden';
 
     try {
       const response = await fetch('https://manu-kapolke.developerakademie.net/portfolio/send_mail.php', {
         method: 'POST',
-        body: formData
+        body: this.getFormData()
       });
 
       if (response.ok) {
-        // Request was successful, handle response here
         console.log('Mail sent successfully');
       } else {
-        // Request failed, handle the error
         console.error('Mail sending failed:', response.statusText);
       }
     } catch (error) {
-      // Network error or other exceptions
       console.error('An error occurred:', error);
     }
 
-    // Text anzeigen: Nachricht gesendet
-    // todo...
+    setTimeout(() => {
+      this.messageIsSent = true;
+    }, 1200);
+  }
 
-    this.messageIsSending = false;
+  getFormData(): FormData {
+    let formData = new FormData();
+    formData.append('name', this.contactForm.get('name')?.value);
+    formData.append('email', this.contactForm.get('email')?.value);
+    formData.append('message', this.contactForm.get('message')?.value);
+    return formData;
+  }
+
+  closeSendingScreen(): void {
+    this.sending = false;
+    this.messageIsSent = false;
+    document.body.style.overflowY = 'unset';
     this.contactForm.enable();
+  }
+
+  invalidInput(name: string): boolean {
+    return name == 'email' ?
+      this.contactForm.get(name)?.value.length > 0 && this.contactForm.get(name)?.hasError('email') || false :
+      this.contactForm.get(name)?.value.length > 0 && this.contactForm.get(name)?.invalid && !this.contactForm.disabled || false;
+  }
+
+  validInput(name: string): boolean {
+    return this.contactForm.get(name)?.valid || false;
   }
 }

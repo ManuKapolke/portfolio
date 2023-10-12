c52be4e6ae81c51bcc7f224e05f5863da7acb920
Manu Kapolke <manukapolke@gmail.com>
2023-10-12 17:49:04 +0200

    mobile-menu opening via css

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  // @ViewChild('toggleBurgerMenu') toggleBurgerMenu?: HTMLInputElement;
 
+  isChecked: boolean = false;
+  scrollTop: number = 0;
+
+  toggleBodyOverflow() {
+    if (this.isChecked) {
+      this.closeMobileMenu();
+    } else {
+      this.scrollTop = document.documentElement.scrollTop;
+      document.body.style.setProperty('--scrollTop', -(this.scrollTop) + "px");
+      document.body.classList.add('no-scrolling');
+      this.isChecked = true;
+    }
+  }
+
+  closeMobileMenu() {
+    document.body.classList.remove('no-scrolling');
+    document.documentElement.style.scrollBehavior = 'auto';
+    document.documentElement.scrollTop = this.scrollTop;
+    document.documentElement.style.scrollBehavior = 'smooth';
+    this.isChecked = false;
+  }
 }

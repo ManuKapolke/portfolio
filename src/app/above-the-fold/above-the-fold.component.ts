fb43df4df93b6155d97050c44f2e7a38a10ac083
Manu Kapolke <manukapolke@gmail.com>
2023-11-13 21:46:06 +0100

    main-title-left translation and font-size

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -12,7 +12,6 @@ export class AboveTheFoldComponent {
 
   constructor(public layoutService: LayoutService, public translate: TranslateService) {
     translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      // console.log(event);
       this.setSubTitle(event.lang);
     });
 
@@ -25,5 +24,22 @@ export class AboveTheFoldComponent {
     else if (language === 'de') {
       this.subTitle = Array.from('FRONTEND ENTWICKLER');
     }
+  }
+
+  getFontSizeByLanguage(): object {
+    let fontSize: string;
+    let letterSpacing: string;
+    if (window.innerWidth < 500) {
+      fontSize = this.translate.currentLang === 'de' ? 'clamp(12px, 4vw, 20px)' :
+        'clamp(16px, 6vw, 34px)';
+      letterSpacing = this.translate.currentLang === 'de' ? 'clamp(0px, 0.04vw, 1px)' :
+        'clamp(1.5px, 0.5vw, 2.5px)';
+    } else {
+      fontSize = this.translate.currentLang === 'de' ? 'clamp(24px, 2vw, 36px)' :
+        'clamp(34px, 3vw, 50px)';
+      letterSpacing = this.translate.currentLang === 'de' ? 'clamp(0px, 0.04vw, 1px)' :
+        'clamp(2.5px, 0.24vw, 4px)';
+    }
+    return { 'font-size': fontSize, 'letter-spacing': letterSpacing };
   }
 }
